8a8696ebbc35b1ee6dbadf384080f26ef1bd48db
fredecampinez <fredecamp@icloud.com>
2020-05-21 12:07:22 +0200

    Acabado método de Tree 21May 12.07

--- src/domain/Tree.java
+++ src/domain/Tree.java
@@ -1,5 +1,21 @@
 package domain;
 
-public class Tree {
+public class Tree extends Product {
+	float heigh;
 
+	public Tree(int code, String name, String nameProduct, float price, int stock, float heigh) {
+		
+		super( code,  name,  price,  stock);
+		this.heigh=heigh;
+	}
+
+	public  float getHeigh() {
+		return heigh;
+	}
+
+	public  void setHeigh(float heigh) {
+		this.heigh = heigh;
+	}
+
+	
 }
