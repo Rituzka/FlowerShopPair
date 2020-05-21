a0aa94f6584b2bc2c1dcf80c2cc96da8904c0389
fredecampinez <fredecamp@icloud.com>
2020-05-21 18:24:59 +0200

    Metodos de Repository con validación 21May20 18:24

--- src/persistence/Repository.java
+++ src/persistence/Repository.java
@@ -1,5 +1,29 @@
 package persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Product;
+
 public class Repository {
 
-}
+	static ArrayList <Product> items= new ArrayList <Product>();
+	
+	public Repository() {
+	
+	}
+
+	public static  ArrayList<Product> getItems() {
+		return items;
+	}
+
+	public static  void addItems(Product items) throws Exception {
+		
+		if (items==null)
+			throw new Exception();
+		
+		((List<Product>) items).add(items);
+				
+	}
+	
+ }
